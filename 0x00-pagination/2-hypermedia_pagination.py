c65411f08b6e2adbbef8ce15cad24849c3313dc7
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-06-23 11:43:42 +0100

    adding all files

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -40,7 +40,7 @@ class Server:
             return []
         return ans[first:last]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> object:
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         '''this is an improve version'''
         res = self.get_page(page, page_size)
         total_pages = math.ceil(len(self.dataset()) / page_size)
