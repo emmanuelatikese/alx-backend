053dea552d71344433e4fd724763c071c4de965c
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-06-26 19:23:16 +0100

    adding

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -14,5 +14,6 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         '''getting as always'''
-        return self.cache_data[key] if key in self.cache_data or\
-            key is not None else None
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
