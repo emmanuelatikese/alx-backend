01bdce5fe7b470d349d37d1a771c18203043a245
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-06-26 10:37:16 +0100

    adding

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+'''least recently used'''
+from collections import OrderedDict
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    '''the class begins here'''
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        '''discard the last item'''
+        if not key and item is None:
+            return
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key)
+        if len(self.cache_data) > self.MAX_ITEMS:
+            _key = list(self.cache_data.keys())[0]
+            self.cache_data.pop(_key)
+            print("DISCARD: {}".format(_key))
+
+    def get(self, key):
+        '''getting as always'''
+        if key not in self.cache_data or key is None:
+            return None
+        self.cache_data.move_to_end(key)
+        return self.cache_data[key]
