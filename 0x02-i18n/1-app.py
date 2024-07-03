961794d103804ae8a3256a507ba6271a1cd474b1
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-03 01:44:16 +0100

    adding

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,16 +4,15 @@ from flask import Flask, render_template
 from flask_babel import Babel
 
 
+class Config:
+    '''all about config settings'''
+    app.config['BABEL_DEFAULT_LOCALE'] = 'en-US'
+    app.config['BABEL_SUPPORTED_LOCALES'] = ["en", "fr"]
+    app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
 
 app = Flask(__name__)
 babel = Babel(app)
-
-app.config['BABEL_DEFAULT_LOCALE'] = 'en-US'
-app.config['BABEL_SUPPORTED_LOCALES'] = ["en", "fr"]
-app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
-
-
-
+app.config.from_object(Config)
 
 @app.route('/')
 def index():
