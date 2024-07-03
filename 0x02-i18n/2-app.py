cfe4a1241d21e80715ecdce325538ec9bc4897a8
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-03 01:54:43 +0100

    adding

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -4,21 +4,27 @@ from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
+class Config:
+    '''all about config settings'''
+    BABEL_DEFAULT_LOCALE = 'en-US'
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
 app = Flask(__name__)
 babel = Babel(app)
-
-app.config['BABEL_DEFAULT_LOCALE'] = 'en-US'
-app.config['BABEL_SUPPORTED_LOCALES'] = ["en", "fr"]
-app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
 
 
 @babel.localeselector
-def get_locale():
-    return request.accept_languages.best_match(app.config['BABEL_SUPPORTED_LOCALES'])
+def get_locale() -> str:
+    '''getting locale'''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
-def index():
+def index() -> str:
     '''first page start here'''
     return render_template('2-index.html')
 
