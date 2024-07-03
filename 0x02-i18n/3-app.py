ec735f9618b0632339ec06533517f977b658be41
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-03 01:59:25 +0100

    adding

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -4,25 +4,34 @@ from flask import Flask, render_template, request
 from flask_babel import Babel, gettext
 
 
+class Config:
+    '''all about config settings'''
+    BABEL_DEFAULT_LOCALE = 'en-US'
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
 app = Flask(__name__)
 babel = Babel(app)
-
-app.config['BABEL_DEFAULT_LOCALE'] = 'en-US'
-app.config['BABEL_SUPPORTED_LOCALES'] = ["en", "fr"]
-app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+app.config.from_object(Config)
 
 
 @babel.localeselector
-def get_locale():
-    return request.accept_languages.best_match(app.config['BABEL_SUPPORTED_LOCALES'])
+def get_locale() -> str:
+    '''getting locale information'''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
-def index():
+def index() -> str:
     '''first page start here'''
     home_title = gettext("Welcome to Holberton")
     home_header = gettext("Hello world!")
-    return render_template('3-index.html', home_header=home_header, home_title=home_title)
+    return render_template(
+        '3-index.html',
+        home_header=home_header,
+        home_title=home_title
+        )
 
 
 if __name__ == '__main__':
